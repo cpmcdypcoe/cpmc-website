1b0aa44bef7d2d0b138efdf2a50e89451a6db5b5
Aditya Bisht <adityabisht894@gmail.com>
2024-08-04 11:09:12 +0530

    pics uploaded

--- components/assets/Team/Data/2k24Data.js
+++ components/assets/Team/Data/2k24Data.js
@@ -1,35 +1,50 @@
-import aj from '../aj.png';
-import palash from '../palash.jpeg';
-import sarthak from '../sarthak.jpeg';
-import sourabh from '../sourabh.jpeg';
-import bk from '../bhushan.jpeg';
-import nishant from '../nishant.jpeg';
-import mrunmayi from '../mrunmayi.jpeg';
-import sapna from '../sapna.jpeg';
-import shorya from '../shorya.JPG';
-import arya from '../arya.jpeg';
-import saurabh from '../saurabh.jpg';
-import pratikPatil from './gunners/pratikPatil.jpg';
-import aihrar from './gunners/aihrar.png';
-import rutik from './gunners/rutik.png';
-import surajPisal from './gunners/surajPisal.jpg';
-import vedant from './gunners/vedant.jpg';
-import pranavJoshi from './gunners/pranavJoshi.jpg';
-import ameyswami from './gunners/ameyswami.jpg';
-import sukrut from './gunners/sukrut.jpg';
-import rasika from './gunners/rasika.jpg';
-import princeton from './gunners/princeton.jpg';
-import adityaBisht from './gunners/adityaBisht.jpg';
-import harshalRudra from './gunners/harshalrudra.jpg';
-import sanyog from './gunners/sanyog.JPG';
-import atharv from './gunners/atharvMuley.jpg';
+import atharvsir from '../atharvsir.jpg';
+import aditya from '../aditya.jpeg';
+import zoro from '../zoro.jpg'
+import vedant from '../vedant.jpg'
+import jaya from '../jayesh.jpg'
+import aftu from '../aftab.jpeg'
+import shroff from '../kshitij.jpeg'
+import khetre from '../prathamesh.png'
+import aayush from '../aayush.jpg'
+import praful from '../praful.jpg'
+
+import shweta from '../Data/members/shweta.jpg'
+import saurabh from '../Data/members/saurabh.jpeg'
+import pranav from '../Data/members/pranav.jpg'
+import mayank from '../Data/members/mayank.jpeg'
+
+import dj from '../Data/members/dj.jpg'
+import jd from '../Data/members/jd.jpg'
+import dinesh from '../Data/members/dinesh.png'
+import sanjana from '../Data/members/sanjana.jpg'
+import jagdish from '../Data/members/jagdish.jpg'
+import prajwal  from '../Data/members/dj.jpg'
+
+import khushi from '../Data/members/khushi.jpg'
+import kishor from '../Data/members/kishor.jpg'
+import uday from '../Data/members/uday.png'
+import aniket from '../Data/members/aniket.jpg'
+import parth from '../Data/members/parth.jpeg'
+import ritika from '../Data/members/ritika.jpg'
+
+import shruti from '../Data/members/shruti.jpg'
+import aaryan from '../Data/members/aaryan.jpg'
+import nutun from '../Data/members/nutun.jpg'
+
+import anisha from '../Data/members/anisha.jpg'
+import sanket from '../Data/members/sanket.jpg'
+import atharva from '../Data/members/atharva.jpg'
+import tejveer from '../Data/members/tejveer.jpg'
+import tejas from '../Data/members/tejas.jpeg'
+
 
 export const Admiral = [
   {
     name: 'Atharv Mulay',
     position: 'Admiral',
     class: '(BE-IT)',
-    photo: aj,
+    photo: atharvsir,
     linkedin: 'https://www.linkedin.com/in/atharva-muley-',
     instagram: 'https://www.instagram.com/_atharva_muley_/',
     github: 'https://github.com/Xtatic-13'
@@ -40,7 +55,7 @@ export const ViceAdmmiral = [
     name: 'Aditya Bisht',
     position: 'Vice-Admiral',
     class: '(BE-RNA)',
-    photo: palash,
+    photo: aditya,
     linkedin: 'https://www.linkedin.com/in/aditya894/',
     instagram: 'https://www.instagram.com/aditya__894/',
     github: 'https://github.com/adityab894'
@@ -52,7 +67,7 @@ export const commander = [
     name: 'Zunzarrao Deore',
     position: 'Commander',
     class: '(BE-AI&DS)',
-    photo: mrunmayi,
+    photo: zoro,
     linkedin: 'https://www.linkedin.com/in/zunzarrao-deore-367b8b1b9/',
     instagram: 'https://www.instagram.com/zunzarrao_deore._/',
     github: 'https://github.com/zunzar1'
@@ -61,7 +76,7 @@ export const commander = [
     name: 'Vedant Bodkhe',
     position: 'Commander',
     class: '(BE-IT)',
-    photo: mrunmayi,
+    photo: vedant,
     linkedin: 'https://www.linkedin.com/in/vedant-bodkhe-141822226/',
     instagram: 'https://www.instagram.com/vedant._.bodkhe/',
     github: ''
@@ -70,7 +85,7 @@ export const commander = [
     name: 'Jayesh Badgujar',
     position: 'Commander',
     class: '(BE-IT)',
-    photo: mrunmayi,
+    photo: jaya,
     linkedin: 'https://www.linkedin.com/in/jayeshbadgujar',
     instagram: 'https://www.instagram.com/jayesh_badgujar_',
     github: 'https://github.com/j4yesh'
@@ -79,7 +94,7 @@ export const commander = [
     name: 'Aftab Mulani',
     position: 'Commander',
     class: '(BE-CS)',
-    photo: mrunmayi,
+    photo: aftu,
     linkedin: 'https://www.linkedin.com/in/aftab-mulani-a81456188/',
     instagram: 'https://www.instagram.com/aftabm2107',
     github: 'https://github.com/Aftab-M'
@@ -88,7 +103,7 @@ export const commander = [
     name: 'Kshitij Shroff',
     position: 'Commander',
     class: '(SE-CS)',
-    photo: mrunmayi,
+    photo: shroff,
     linkedin: 'https://www.linkedin.com/in/kshitij-shroff-3a79782a4/',
     instagram: '',
     github: 'https://github.com/Kshitij-hub23'
@@ -97,7 +112,7 @@ export const commander = [
     name: 'Prathamesh Khetre',
     position: 'Commander',
     class: '(BE-CS)',
-    photo: mrunmayi,
+    photo: khetre,
     linkedin: 'https://www.linkedin.com/in/prathamesh-khetre-838450225/',
     instagram: 'https://www.instagram.com/prathameshk_94/',
     github: 'https://github.com/Prathamesh942'
@@ -106,7 +121,7 @@ export const commander = [
     name: 'Praful Raghorte',
     position: 'Commander',
     class: '(BE-E&TC)',
-    photo: mrunmayi,
+    photo: praful,
     linkedin: 'https://www.linkedin.com/in/praful-raghorte-6ba851231/',
     instagram: 'https://www.instagram.com/_praful_op/',
     github: ''
@@ -115,7 +130,7 @@ export const commander = [
     name: 'Ayush Gandhewar',
     position: 'Commander',
     class: '(BE-AI&DS)',
-    photo: mrunmayi,
+    photo: aayush,
     linkedin: 'https://www.linkedin.com/in/ayush-gandhewar-126448241/',
     instagram: 'https://www.instagram.com/_ayxshhh',
     github: 'https://github.com/Ayushgandhewar'
@@ -127,7 +142,7 @@ export const spankars = [
     name: 'Shweta Shinde',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: shweta,
     linkedin: 'https://www.linkedin.com/in/shweta-shinde-7557b325b/',
     instagram: '',
     github: ''
@@ -136,7 +151,7 @@ export const spankars = [
     name: 'Saurabh Mandal',
     position: 'Gunner',
     class: '(BE-IT)',
-    photo: pratikPatil,
+    photo: saurabh,
     linkedin: 'https://www.linkedin.com/in/saurabh-mandal-b03305208/',
     instagram: 'https://www.instagram.com/_mandal_saurabh_/',
     github: 'https://github.com/saurabh-mandal-29'
@@ -145,7 +160,7 @@ export const spankars = [
     name: 'Mayank Sukhdeve',
     position: 'Gunner',
     class: '(BE-IT)',
-    photo: pratikPatil,
+    photo: mayank,
     linkedin: 'https://www.linkedin.com/in/mayank-sukhadeve/',
     instagram: 'https://www.instagram.com/mayank.sukhadeve',
     github: 'https://github.com/Myank-sic'
@@ -154,7 +169,7 @@ export const spankars = [
     name: 'Pranav Kale',
     position: 'Gunner',
     class: '(BE-AI&DS)',
-    photo: pratikPatil,
+    photo: pranav,
     linkedin: 'https://www.linkedin.com/in/kale-pranav/',
     instagram: '',
     github: 'https://github.com/PranavKale03'
@@ -166,7 +181,7 @@ export const endeavours = [
     name: 'Sanjana Varade',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: sanjana,
     linkedin: 'https://www.linkedin.com/in/sanjana-varade-b52312250/',
     instagram: 'https://www.instagram.com/sanjanavarade/',
     github: 'https://github.com/sanjanavarade'
@@ -175,7 +190,7 @@ export const endeavours = [
     name: 'Janhavi Dahatonde',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: jd,
     linkedin: 'https://www.linkedin.com/in/janhavi2409/',
     instagram: 'https://www.instagram.com/janhavid_2409/',
     github: 'https://github.com/Janhavi2409'
@@ -184,7 +199,7 @@ export const endeavours = [
     name: 'Prajwal Sanap',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: prajwal,
     linkedin: 'https://www.linkedin.com/in/prajwalsanap/',
     instagram: 'https://www.instagram.com/sanap_prajwal/',
     github: 'https://github.com/sandy787'
@@ -193,7 +208,7 @@ export const endeavours = [
     name: 'Digvijay Mangaonkar',
     position: 'Gunner',
     class: '(TE-CS)',
-    photo: pratikPatil,
+    photo: dj,
     linkedin: 'https://www.linkedin.com/in/dj-mangaonkar-07k/',
     instagram: 'https://www.instagram.com/_dj_m07_/',
     github: 'https://github.com/DJ-BoT07'
@@ -202,7 +217,7 @@ export const endeavours = [
     name: 'Dinesh Aher',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: dinesh,
     linkedin: 'https://www.linkedin.com/in/dineshaher/',
     instagram: '',
     github: 'https://github.com/Dinesh-dypcoe'
@@ -211,7 +226,7 @@ export const endeavours = [
     name: 'Jagdish Raut',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: jagdish,
     linkedin: 'https://www.linkedin.com/in/jagdish-raut-bba036259/',
     instagram: 'https://www.instagram.com/jagdish.2004/',
     github: 'https://github.com/Jagdish2004'
@@ -223,7 +238,7 @@ export const lanisters = [
     name: 'Parth Shinde',
     position: 'Gunner',
     class: '(TE-AI&DS)',
-    photo: pratikPatil,
+    photo: parth,
     linkedin: 'https://www.linkedin.com/in/parth-shinde-4692ba259',
     instagram: '',
     github: ''
@@ -232,7 +247,7 @@ export const lanisters = [
     name: 'Aniket Mane',
     position: 'Gunner',
     class: '(BE-E&TC)',
-    photo: pratikPatil,
+    photo: aniket,
     linkedin: 'https://www.linkedin.com/in/maneaniket/',
     instagram: 'https://www.instagram.com/aniket_mane_45/',
     github: 'https://github.com/maneaniket2003'
@@ -241,7 +256,7 @@ export const lanisters = [
     name: 'Uday Bhole',
     position: 'Gunner',
     class: '(TE-AI&DS)',
-    photo: pratikPatil,
+    photo: uday,
     linkedin: 'https://www.linkedin.com/in/uday-bhole-454568254/',
     instagram: '',
     github: 'https://github.com/udaybhole'
@@ -250,7 +265,7 @@ export const lanisters = [
     name: 'Khushi Sanjay',
     position: 'Gunner',
     class: '(TE-AI&DS)',
-    photo: pratikPatil,
+    photo: khushi,
     linkedin: 'https://www.linkedin.com/in/khushi-sanjay-22b665273/',
     instagram: '',
     github: ''
@@ -259,7 +274,7 @@ export const lanisters = [
     name: 'Kishor Mote',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: kishor,
     linkedin: 'https://www.linkedin.com/in/kishor-mote-a63988257/',
     instagram: 'https://www.instagram.com/kishor.mote.1428',
     github: 'https://github.com/kishormote'
@@ -268,7 +283,7 @@ export const lanisters = [
     name: 'Ritika Baranwal',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: ritika,
     linkedin: 'https://www.linkedin.com/in/ritika-baranwal-72031b259/',
     instagram: '',
     github: 'https://github.com/ritikabaranwal1006'
@@ -280,7 +295,7 @@ export const malignant = [
     name: 'Shruti Dikkar',
     position: 'Gunner',
     class: '(BE-CS)',
-    photo: pratikPatil,
+    photo: shruti,
     linkedin: 'https://www.linkedin.com/in/shrutidikkar/',
     instagram: '',
     github: 'https://github.com/Shrutidikkar30'
@@ -289,7 +304,7 @@ export const malignant = [
     name: 'Aaryan Palve',
     position: 'Gunner',
     class: '(TE-AI&DS)',
-    photo: pratikPatil,
+    photo: aaryan,
     linkedin: 'https://www.linkedin.com/in/aaryan-palve-6124372b7/',
     instagram: 'https://www.instagram.com/aaryanpalve2004',
     github: 'https://github.com/AaryanPalve5'
@@ -298,7 +313,7 @@ export const malignant = [
     name: 'Nutun Gaikwad',
     position: 'Gunner',
     class: '(BE-AI&DS)',
-    photo: pratikPatil,
+    photo: nutun,
     linkedin: 'https://www.linkedin.com/in/nutan-gaikwad-4ba854196/',
     instagram: 'https://www.instagram.com/n.g_8917/',
     github: 'https://github.com/amante-8917'
@@ -310,7 +325,7 @@ export const communityMembers = [
     name: 'Anisha Mishra',
     position: 'Gunner',
     class: '(TE-AI&DS)',
-    photo: pratikPatil,
+    photo: anisha,
     linkedin: 'https://www.linkedin.com/in/anisha-mishra-820236281/',
     instagram: 'https://www.instagram.com/anishamishra_15',
     github: ''
@@ -319,7 +334,7 @@ export const communityMembers = [
     name: 'Sanket Natekar',
     position: 'Gunner',
     class: '(TE-CS)',
-    photo: pratikPatil,
+    photo: sanket,
     linkedin: 'https://www.linkedin.com/in/sanket-natekar/',
     instagram: 'https://www.instagram.com/sanketnatekar_1414/',
     github: 'https://github.com/SanketNatekar'
@@ -328,7 +343,7 @@ export const communityMembers = [
     name: 'Atharva Jagtap',
     position: 'Gunner',
     class: '(SE-CS)',
-    photo: pratikPatil,
+    photo: atharva,
     linkedin: 'https://www.linkedin.com/in/atharva-jagtap112/',
     instagram: 'https://www.instagram.com/_heyatharva/',
     github: 'https://github.com/atharvajagtap112'
@@ -337,7 +352,7 @@ export const communityMembers = [
     name: 'Tejveer Singh',
     position: 'Gunner',
     class: '(TE-IT)',
-    photo: pratikPatil,
+    photo: tejveer,
     linkedin: 'https://www.linkedin.com/in/tejveer-singh-2742bb259/',
     instagram: 'https://www.instagram.com/charms.letyou/',
     github: 'https://github.com/tejveer7918'
@@ -346,7 +361,7 @@ export const communityMembers = [
     name: 'Tejas Bachhav',
     position: 'Gunner',
     class: '(BE-AI&DS)',
-    photo: pratikPatil,
+    photo: tejas,
     linkedin: 'https://www.linkedin.com/in/tejas-bachhav-488a2a233/',
     instagram: 'https://www.instagram.com/tejasbachhav11/',
     github: 'https://github.com/TejasBachhav11'
