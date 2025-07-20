1a66ccf53f197c548cdc593c083d9fd7cc196d8e
alisha <alishasapkal@gmail.com>
2025-07-20 12:39:22 +0530

    2k23 added back

--- components/Teams/Teams.jsx
+++ components/Teams/Teams.jsx
@@ -10,6 +10,17 @@ const Teams = () => {
       </h1>
       <div className="flex gap-2">
         <div className="">
+          <Link href="/team/2k23">
+            <button className={classes.btn}>
+              <span className={classes.span}>2k23</span>
+              <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </button>
+          </Link>
+        </div>
+        <div className="">
           <Link href="/team/2k24">
             <button className={classes.btn}>
               <span className={classes.span}>2k24</span>
